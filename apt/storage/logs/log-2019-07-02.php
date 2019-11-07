be7dc633d289724b7866705d5b27ea482739a5b8
saintjk <jkjayakrishnan70@gmail.com>
2019-11-07 10:56:38 +0530

    Project Start
    Project Start

---
+++ apt/storage/logs/log-2019-07-02.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-02 12:21:40 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2019-07-02 12:21:40 --> #0 C:\xampp\htdocs\apt\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\apt\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\apt\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
+ERROR - 2019-07-02 12:36:29 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2019-07-02 12:36:29 --> #0 C:\xampp\htdocs\apt\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\apt\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\apt\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
+ERROR - 2019-07-02 13:11:19 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2019-07-02 13:11:19 --> #0 C:\xampp\htdocs\apt\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\apt\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\apt\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
+ERROR - 2019-07-02 14:01:09 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2019-07-02 14:01:09 --> #0 C:\xampp\htdocs\apt\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\apt\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\apt\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
---
+++ apt/storage/logs/log-2019-08-22.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-08-22 09:07:22 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2019-08-22 09:07:22 --> #0 C:\xampp\htdocs\apt\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\apt\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\apt\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
